c87077061ce00041e0a07bade4dc44622b2f8e99
Sergey <seregaryz@gmail.com>
2020-08-18 00:28:42 +0300

    Add camera request permission and camera capture session

--- app/src/main/java/com/kpfu/itis/customcameraproject/CameraFragment.kt
+++ app/src/main/java/com/kpfu/itis/customcameraproject/CameraFragment.kt
@@ -1,20 +1,49 @@
 package com.kpfu.itis.customcameraproject
 
+import android.Manifest
+import android.content.Context
 import android.graphics.SurfaceTexture
+import android.hardware.camera2.*
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.TextureView
-import android.view.View
-import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_camera.*
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+import java.lang.IllegalArgumentException
+import java.util.*
 
 class CameraFragment : Fragment() {
-    private val ARG_PARAM1 = "param1"
-    private val ARG_PARAM2 = "param2"
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private val MAX_PREVIEW_WIDTH = 1920
+    private val MAX_PREVIEW_HEIGHT = 1080
+    private lateinit var captureRequestBuilder: CaptureRequest.Builder
+    private lateinit var captureSession: CameraCaptureSession
+    private lateinit var cameraDevice: CameraDevice
+    private val deviceStateCallback = object: CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            Log.d(TAG, "camera device opened")
+            cameraDevice = camera
+            previewSession()
+        }
+
+        override fun onDisconnected(camera: CameraDevice) {
+            Log.d(TAG, "camera device disconnected")
+            camera.close()        }
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            Log.d(TAG, "camera device error")
+            this@CameraFragment.activity?.finish()        }
+
+    }
+    private val cameraManager by lazy {
+        activity?.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    }
+    private lateinit var backgroundThread: HandlerThread
+    private lateinit var backgroundHandler: Handler
 
     private val surfaceListener = object: TextureView.SurfaceTextureListener{
         override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
@@ -34,6 +63,7 @@ class CameraFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
+        startBackgroundThread()
         if (cameraTextureView.isAvailable){
             openCamera()
         } else {
@@ -41,31 +71,129 @@ class CameraFragment : Fragment() {
         }
     }
 
-    private fun openCamera(){
-
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onPause() {
+        closeCamera()
+        stopBackgroundThread()
+        super.onPause()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.fragment_camera, container, false)
 
-    companion object {
+    private fun startBackgroundThread() {
+        backgroundThread = HandlerThread("Camera2 Kotlin").also { it.start() }
+        backgroundHandler = Handler(backgroundThread.looper)
+    }
 
+    private fun stopBackgroundThread() {
+        backgroundThread.quitSafely()
+
+        try {
+            backgroundThread.join()
+        } catch (e: InterruptedException) {
+            Log.e(TAG, e.toString())
+        }
+    }
+
+    private fun openCamera(){
+        checkCameraPermission()
+    }
+
+    private fun closeCamera() {
+        if (this::captureSession.isInitialized)
+            captureSession.close()
+        if (this::cameraDevice.isInitialized)
+            cameraDevice.close()
+    }
+
+    private fun previewSession() {
+        try {
+            val surfaceTexture = cameraTextureView.surfaceTexture
+            surfaceTexture.setDefaultBufferSize(MAX_PREVIEW_WIDTH, MAX_PREVIEW_HEIGHT)
+            val surface = Surface(surfaceTexture)
+            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            captureRequestBuilder.addTarget(surface)
+            cameraDevice.createCaptureSession(
+                listOf(surface),
+                object: CameraCaptureSession.StateCallback() {
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+                        Log.e(TAG, "Failed to create capture session")
+                    }
+
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        captureSession = session
+                        try {
+                            captureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
+                            captureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null)
+                        } catch (e: CameraAccessException) {
+                            Log.e(TAG, e.toString())
+                        }
+                    }
+
+                }, null)
+
+        } catch (e: CameraAccessException) {
+            Log.e(TAG, e.toString())
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    @AfterPermissionGranted(REQUEST_CAMERA_PERMISSION)
+    private fun checkCameraPermission(){
+        if(EasyPermissions.hasPermissions(activity!!, Manifest.permission.CAMERA)) {
+            Log.d(TAG, "App has camera permission")
+            connectCamera()
+        } else {
+            EasyPermissions.requestPermissions(activity!!,
+                getString(R.string.camera_request_tarionale),
+                REQUEST_CAMERA_PERMISSION, Manifest.permission.CAMERA)
+        }
+    }
+
+    private fun <T> cameraCharacteristics(cameraId: String, key: CameraCharacteristics.Key<T>) : T? {
+        val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+        return when (key) {
+            CameraCharacteristics.LENS_FACING -> characteristics.get(key)
+            CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP -> characteristics.get(key)
+            else -> throw IllegalArgumentException("Key not recognized")
+        }
+    }
+
+    private fun getCameraId(lens: Int) :String {
+        var deviceId = listOf<String>()
+        try {
+            val cameraIdList = cameraManager.cameraIdList
+            deviceId = cameraIdList.filter { lens == cameraCharacteristics(it, CameraCharacteristics.LENS_FACING) }
+        } catch (e: CameraAccessException) {
+            Log.e(TAG, e.toString())
+        }
+        return deviceId[0]
+    }
+
+    private fun connectCamera() {
+        val deviceId = getCameraId(CameraCharacteristics.LENS_FACING_BACK)
+        Log.d(TAG, "deviceId: $deviceId")
+        try {
+            cameraManager.openCamera(deviceId, deviceStateCallback, backgroundHandler)
+        } catch (e: CameraAccessException) {
+            Log.e(TAG, e.toString())
+        } catch (e: InterruptedException) {
+            Log.e(TAG, "Open camera device interrupted while opened")
+        }
+    }
+
+    companion object {
+        const val REQUEST_CAMERA_PERMISSION = 100
         private val TAG = CameraFragment::class.qualifiedName
-        fun newInstance(param1: String, param2: String) =
-            CameraFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance(param1: String, param2: String) = CameraFragment()
     }
 }
